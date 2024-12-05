03bd77696a9bb4e8b39f1b0606ce796d684505d1
iiamnimx <nimishaghosh402@gmail.com>
2024-12-05 20:03:38 +0530

    Add files via upload

---
+++ add.js
@@ -0,0 +1,4 @@
+function calculate() {
+    var f1 = parseInt(document.getElementById("fnumber").value)
+    var f1 = parsedocument.getElementById("fnumber").value
+    document.getElementById("result")
